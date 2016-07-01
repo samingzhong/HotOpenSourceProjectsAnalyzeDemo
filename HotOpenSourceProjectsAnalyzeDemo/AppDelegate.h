a50379a31dbd8fcc151f89590d20d4700f5254dc
钟晓明 <samingzhong@qq.com>
2016-07-01 17:57:29 +0800

    Initial Commit

---
+++ HotOpenSourceProjectsAnalyzeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotOpenSourceProjectsAnalyzeDemo
+//
+//  Created by samingzhong on 16/7/1.
+//  Copyright © 2016年 Sunteng Information Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotOpenSourceProjectsAnalyzeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotOpenSourceProjectsAnalyzeDemo
+//
+//  Created by samingzhong on 16/7/1.
+//  Copyright © 2016年 Sunteng Information Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
